4e7f853553527cc4d77a694ae83aa92f944bcb6c
shivam-goyal-ind <imgoyal2005@gmail.com>
2025-04-14 16:38:54 +0530

    added utils and models

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,19 @@
-import express from 'express';
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 
-const app = express();
+dotenv.config({
+    path: './.env'
+})
 
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-});
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
