f2c54dd47224feab7d2f772739bd884300deac23
shivam-goyal-ind <imgoyal2005@gmail.com>
2025-04-17 10:00:59 +0530

    user registration and login handlings done

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -10,5 +11,9 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieParser())
+
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users", userRouter)
 
 export { app }
---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    getCurrentUser, 
+    getSolveHistory, 
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/history").get(verifyJWT, getSolveHistory)
+
+export default router
